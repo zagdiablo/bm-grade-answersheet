2077d5994ec9eda2c00c15da29475c2dc50572df
Adhitia Nugraha <adhitia.nugrahatkj@gmail.com>
2023-09-01 08:55:32 +0700

    finished database models design

--- app/models.py
+++ app/models.py
@@ -18,6 +18,25 @@ class User(db.Model, UserMixin):
 
 class Soal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    nama_soal = db.Column(db.String(300), nullable=False, default='Untitled soal')
+    nama_soal = db.Column(db.String(300), nullable=False, default="Untitled soal")
     link_id = db.Column(db.String(300), nullable=False)
-    
+    qr_code_img = db.Column(db.String(300), nullable=False)
+    jawaban = db.relationship("Jawaban", backref="soal", lazy="default")
+    history_pengerjaan = db.relationship(
+        "HistoryPengerjaan", backref="Soal", lazy="default"
+    )
+
+
+class Jawaban(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nomor = db.Column(db.String(20), nullable=False)
+    jawaban = db.Column(db.String(5), nullable=False)
+    soal_id = db.Column(db.Integer, db.ForeignKey("soal.id"))
+
+
+class HistoryPengerjaan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nama_peserta = db.Column(db.String(300), nullable=False, default="No Name")
+    tanggal = db.Column(db.String(300), nullable=False)
+    jawaban = db.Column(db.String(300), nullable=False)
+    soal_id = db.Column(db.Integer, db.ForeignKey("soal.id"))
